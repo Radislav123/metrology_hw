f3865738aec5cb3945219e0acc990ae1a10c18df
Iftuga <iftuga@ya.ru>
2019-06-07 16:53:57 +0300

    Update data_manipulations.py

--- scripts/service/data_manipulations.py
+++ scripts/service/data_manipulations.py
@@ -6,10 +6,13 @@ def dataload(filename: str):
     Функция принимает строку с расположением файла
 
     :param filename: имя файла
-    :return:
+    :return: возвращает numpy массив
     """
-    data = np.loadtxt(filename, dtype=np.str)
-    for i, value in enumerate(data):
-        data[i] = value.replace(',', '.')
-    data = data.astype(np.float)
+    try:
+        data = np.loadtxt(filename, dtype=np.str)
+        for i, value in enumerate(data):
+            data[i] = value.replace(',', '.')
+        data = data.astype(np.float)
+    except ValueError:
+        print("Мб вы пидор?")
     return data
