3addffbc92ae49965e1c6a71cab67ef6874ee442
cher-di <cher-di@mail.ru>
2019-06-08 13:12:46 +0300

    Added all GUI elements to the root

--- scripts/interface/interface.py
+++ scripts/interface/interface.py
@@ -2,7 +2,14 @@ import tkinter as tk
 from tkinter import filedialog
 from tkinter import messagebox
 from PIL import Image, ImageTk
-import service.data_manipulations as data_manip
+import service.data_manipulations as data_manipulation
+import numpy as np
+import sampleprocessing.somescriptname as data_processing
+
+import matplotlib
+matplotlib.use("TkAgg")
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
 
 
 class _Singleton(type):
@@ -21,17 +28,42 @@ class Window(metaclass=_Singleton):
     """
 
     def __init__(self):
-        self.__initroot()
-        self.__initmenubar()
+        self.__init_root()
+        self.__init_menubar()
 
-        self.__placerootcenter() # это запускается в самом конце
+        self.__data = None
 
-    def __initroot(self):
+        self.__place_root_center()  # это запускается в самом конце
+
+    def __init_root(self):
         self.__root = tk.Tk()
         self.__root.title("Arya")
         self.__root.iconbitmap("../../resources/drawable/cool_icon.ico")
 
-    def __placerootcenter(self):
+        f = Figure(figsize=(5, 5), dpi=100)
+        a = f.add_subplot(111)
+        a.plot([1, 2, 3, 4, 5, 6, 7, 8], [5, 6, 1, 3, 8, 9, 3, 5])
+
+        self.__plot_canvas = FigureCanvasTkAgg(f, self.__root)
+        self.__plot_canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
+        self.__plot_canvas.get_tk_widget().grid(row=0, column=0, rowspan=3)
+
+        self.__status_label = tk.Label(self.__root,
+                                       text=u"Выборка не загружена",
+                                       font="Arial 10",
+                                       bg="red")
+        self.__status_label.grid(row=0, column=1)
+
+        self.__analyze_btn = tk.Button(self.__root,
+                                       text=u"Анализ",
+                                       font="Arial 10",
+                                       state=tk.DISABLED)
+        self.__analyze_btn.grid(row=1, column=1)
+
+        self.__analyze_result_text = tk.Text(self.__root)
+        self.__analyze_result_text.grid(row=2, column=1)
+
+    def __place_root_center(self):
         """
         Располагает root в центре экрана
 
@@ -39,10 +71,13 @@ class Window(metaclass=_Singleton):
         """
         ws, hs = self.__root.winfo_screenwidth(), self.__root.winfo_screenheight()
         w, h = self.__root.winfo_width(), self.__root.winfo_height()
+        print(w, h)
         x, y = ws / 2 - w / 2, hs / 2 - h / 2
         self.__root.geometry('%dx%d+%d+%d' % (w, h, x, y))
+        # self.__root.resizable(False, False)
+        # TODO разберись с размерами root
 
-    def __initmenubar(self):
+    def __init_menubar(self):
         """
         Инициализация меню-бара и всех пунктов меню
         Вызывать только после инициализации root
@@ -54,17 +89,17 @@ class Window(metaclass=_Singleton):
         self.__root.iconbitmap("../../resources/drawable/cool_icon.ico")
 
         self.__filemenu = tk.Menu(self.__menubar, tearoff=0)
-        self.__filemenu.add_command(label=u"Загрузить из файла", command=self.__loadfromfile)
+        self.__filemenu.add_command(label=u"Загрузить из файла", command=self.__load_from_file)
         self.__filemenu.add_command(label=u"Выход", command=self.__root.quit)
 
         self.__helpmenu = tk.Menu(self.__menubar, tearoff=0)
-        self.__helpmenu.add_command(label=u"Помощь", command=self.__showhelp)
-        self.__helpmenu.add_command(label=u"О программе", command=self.__showabout)
+        self.__helpmenu.add_command(label=u"Помощь", command=self.__show_help)
+        self.__helpmenu.add_command(label=u"О программе", command=self.__show_about)
 
         self.__menubar.add_cascade(label=u"Файл", menu=self.__filemenu)
         self.__menubar.add_cascade(label=u"Помощь", menu=self.__helpmenu)
 
-    def __loadfromfile(self):
+    def __load_from_file(self):
         """
         Запись обработанных результатов в текстовый файл
 
@@ -75,80 +110,85 @@ class Window(metaclass=_Singleton):
                                               filetypes=((u"Текстовый файл", "*.txt"),))
         if filename != '':
             try:
-                print("Загрузка:", data_manip.dataload(filename))  # TODO поменяй на рабочий код
+                self.__data = data_manipulation.dataload(filename)
+                self.__status_label.config(text=u"Выборка загружена\n"
+                                                u"Имя файла: " + filename.split('/')[-1],
+                                           bg="yellow")
             except ValueError:
                 messagebox.showwarning("Ошибка чтения", "Данные в файле не соответствуют формату")
 
-    def __showhelp(self):
+    def __show_help(self):
         """
         Появление модального окна "Помощь"
 
         :return:
         """
-        helpwindow = tk.Toplevel(self.__root)
-        helpwindow.transient(self.__root)
-        helpwindow.grab_set()
-        helpwindow.focus_set()
+        help_window = tk.Toplevel(self.__root)
+        help_window.transient(self.__root)
+        help_window.grab_set()
+        help_window.focus_set()
 
-        helpwindow.title(u"Помощь")
-        helpwindow.iconbitmap("../../resources/drawable/cool_icon.ico")
-        helpwindow.resizable(False, False)
+        help_window.title(u"Помощь")
+        help_window.iconbitmap("../../resources/drawable/cool_icon.ico")
+        help_window.resizable(False, False)
 
-        helplabel = tk.Label(helpwindow,
-                           text="Здесь будет помощь")
-        helplabel.grid(row=0, column=0)
+        help_label = tk.Label(help_window, text="Здесь будет помощь")
+        help_label.grid(row=0, column=0)
 
-        okbtn = tk.Button(helpwindow,
-                          padx=30,
-                          text="OK",
-                          font="Arial 11",
-                          command=helpwindow.destroy)
-        okbtn.grid(row=1, column=0)
+        ok_btn = tk.Button(help_window,
+                           padx=30,
+                           text="OK",
+                           font="Arial 11",
+                           command=help_window.destroy)
+        ok_btn.grid(row=1, column=0)
 
-        helpwindow.wait_window()  # это запускается в самом конце
+        help_window.wait_window()  # это запускается в самом конце
 
-    def __showabout(self):
+    def __show_about(self):
         """
         Появление модального окна "О программе"
 
         :return:
         """
-        aboutwindow = tk.Toplevel(self.__root)
-        aboutwindow.transient(self.__root)
-        aboutwindow.grab_set()
-        aboutwindow.focus_set()
+        about_window = tk.Toplevel(self.__root)
+        about_window.transient(self.__root)
+        about_window.grab_set()
+        about_window.focus_set()
 
-        aboutwindow.title(u"О программе")
-        aboutwindow.iconbitmap("../../resources/drawable/cool_icon.ico")
-        aboutwindow.resizable(False, False)
+        about_window.title(u"О программе")
+        about_window.iconbitmap("../../resources/drawable/cool_icon.ico")
+        about_window.resizable(False, False)
 
-        frame = tk.Frame(aboutwindow)
+        frame = tk.Frame(about_window)
         frame.grid(row=0, column=0)
 
         img = Image.open("../../resources/drawable/house_stark.jpg")
         new_width, new_height = 100, 100
         img = img.resize((new_width, new_height), Image.ANTIALIAS)
         img = ImageTk.PhotoImage(img)
-        panelappsymbol = tk.Label(frame, image=img)
-        panelappsymbol.grid(row=0, column=0, rowspan=2)
+        panel_app_symbol = tk.Label(frame, image=img)
+        panel_app_symbol.grid(row=0, column=0, rowspan=2)
 
-        labelprogramname = tk.Label(frame,
-                                    text="Arya",
-                                    font="Arial 24")
-        labelprogramname.grid(row=0, column=1)
-        labelauthors = tk.Label(frame,
-                                text=u"Авторы: cher-di, Iftuga, radislav123",
-                                font="Arial 12")
-        labelauthors.grid(row=1, column=1)
+        label_program_name = tk.Label(frame,
+                                      text="Arya",
+                                      font="Arial 24")
+        label_program_name.grid(row=0, column=1)
+        label_authors = tk.Label(frame,
+                                 text=u"Авторы: cher-di, Iftuga, radislav123",
+                                 font="Arial 12")
+        label_authors.grid(row=1, column=1)
 
-        okbtn = tk.Button(aboutwindow,
+        okbtn = tk.Button(about_window,
                           padx=30,
                           text="OK",
                           font="Arial 11",
-                          command=aboutwindow.destroy)
+                          command=about_window.destroy)
         okbtn.grid(row=1, column=0)
 
-        aboutwindow.wait_window()  # это запускается в самом конце
+        about_window.wait_window()  # это запускается в самом конце
+
+    def __on_analyze_btn_click(self):
+        pass
 
     def run(self):
         """
@@ -159,5 +199,6 @@ class Window(metaclass=_Singleton):
         """
         self.__root.mainloop()
 
-    def __test(self):
+    @staticmethod
+    def __test():
         print("Test")
