6f0e08f6173b264a9750c1e6c31a3de3c1a86e96
cher-di <cher-di@mail.ru>
2019-06-14 12:30:20 +0300

    Fixed Kolmagorov to Kolmogorov in the main window

--- scripts/interface/interface.py
+++ scripts/interface/interface.py
@@ -75,7 +75,7 @@ class Window(metaclass=_Singleton):
         axes.plot(bins, 1 / (sigma * np.sqrt(2 * np.pi)) *
                   np.exp(- (bins - mu) ** 2 / (2 * sigma ** 2)),
                   linewidth=2, color='red')
-        axes.set_title(u"Проверка на критерий Колмагорова")
+        axes.set_title(u"Проверка на критерий Колмогорова")
 
         self.__plot_canvas = FigureCanvasTkAgg(figure, self.__root)
         self.__plot_canvas.get_tk_widget().pack()
